4568a08bc404bbfb48524e92eb7a41fea50eab43
Blinkg <sendalkejepitkaki@gmail.com>
2026-08-29 03:31:21 +0800

    Reverse some change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,8 +70,8 @@ android {
 
             isEnable = !isBuildingBundle
             reset()
-            include("arm64-v8a")
-            isUniversalApk = false
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
         }
     }
 
