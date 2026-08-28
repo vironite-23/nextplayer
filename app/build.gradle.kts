6cbd185dadb6c5693bd036a825c2f3d74a5830cb
Vironite <princegobleen@gmail.com>
2026-08-29 05:30:01 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         applicationId = "dev.anilbeesetti.nextplayer"
-        versionCode = 73
-        versionName = "0.17.6"
+        versionCode = 72
+        versionName = "0.17.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
